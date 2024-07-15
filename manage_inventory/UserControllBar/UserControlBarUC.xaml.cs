952c452994aee51a62239da1a8e88f9e043ff827
TrunggHiieu281103 <thanhcong15426378@gmail.com>
2024-07-15 23:08:32 +0700

    update custom control bar

--- manage_inventory/UserControllBar/UserControlBarUC.xaml.cs
+++ manage_inventory/UserControllBar/UserControlBarUC.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using manage_inventory.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace manage_inventory.UserControllBar
     /// </summary>
     public partial class UserControlBarUC : UserControl
     {
+        public ControlBarViewModel Viewmodel { get; set; }
         public UserControlBarUC()
         {
             InitializeComponent();
+            this.DataContext = Viewmodel = new ControlBarViewModel();
         }
     }
 }
--- manage_inventory/ViewModel/MainViewModel.cs
+++ manage_inventory/ViewModel/MainViewModel.cs
@@ -4,21 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace manage_inventory.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+
+        public ICommand LoadedWindowCommand { get; set; }
+
         // handle logic for MainWindow.xaml
         public bool IsLoaded = false;
         public MainViewModel()
         {
-            if(!IsLoaded)
-            {
+
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
                 IsLoaded = true;
                 LoginWindow loginWindow = new LoginWindow();
                 loginWindow.ShowDialog();
-            }
+                }
+            );
             
         }
     }
