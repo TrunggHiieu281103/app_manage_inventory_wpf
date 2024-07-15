49e3902cff661024240dcf416ea9283be1ec9375
TrunggHiieu281103 <thanhcong15426378@gmail.com>
2024-07-15 21:37:50 +0700

    update

---
+++ manage_inventory/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace manage_inventory
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ manage_inventory/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace manage_inventory.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // handle logic for MainWindow.xaml
+        public bool IsLoaded = false;
+        public MainViewModel()
+        {
+            if(!IsLoaded)
+            {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+            
+        }
+    }
+}
