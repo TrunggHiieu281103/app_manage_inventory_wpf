6411c548756462e7774bbaabf4eb002c49533102
DuongThinh46 <thinhdnse171879@fpt.edu.vn>
2024-07-16 23:20:51 +0700

    DataProvider and Login

---
+++ manage_inventory/Models/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manage_inventory.Models
+{
+    public class DataProvider
+    {
+        private static DataProvider? _ins;
+        public static DataProvider ins 
+        { 
+            get 
+            { 
+                if (_ins == null) _ins = new DataProvider(); return _ins; 
+            } 
+            set 
+            { 
+                _ins = value; 
+            } 
+        }
+
+        public QuanLyKhoContext DB { get; set; }
+        public DataProvider()
+        {
+            DB = new QuanLyKhoContext(); 
+
+        }
+    }
+}
