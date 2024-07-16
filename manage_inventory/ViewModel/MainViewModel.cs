7c18ddc2a24c04b1147a2b93e9a5f1437c1d0c5e
TrunggHiieu281103 <thanhcong15426378@gmail.com>
2024-07-16 19:21:17 +0700

    update input, output, object, user window

--- manage_inventory/ViewModel/MainViewModel.cs
+++ manage_inventory/ViewModel/MainViewModel.cs
@@ -16,6 +16,10 @@ namespace manage_inventory.ViewModel
         public ICommand UnitCommand { get; set; }
         public ICommand SuplierCommand { get; set; }
         public ICommand CustomerCommand { get; set; }
+        public ICommand ObjectCommand { get; set; }
+        public ICommand UserCommand { get; set; }
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
 
         // handle logic for MainWindow.xaml
         public MainViewModel()
@@ -31,6 +35,10 @@ namespace manage_inventory.ViewModel
             UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) => { UnitWindow wd = new UnitWindow(); wd.ShowDialog(); } );
             SuplierCommand = new RelayCommand<object>((p) => { return true; }, (p) => { SuplierWindow wd = new SuplierWindow(); wd.ShowDialog(); } );
             CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) => { CustomerWindow wd = new CustomerWindow(); wd.ShowDialog(); } );
+            ObjectCommand = new RelayCommand<object>((p) => { return true; }, (p) => { ObjectWindow wd = new ObjectWindow(); wd.ShowDialog(); } );
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) => { UserWindow wd = new UserWindow(); wd.ShowDialog(); } );
+            InputCommand = new RelayCommand<object>((p) => { return true; }, (p) => { InputWindow wd = new InputWindow(); wd.ShowDialog(); } );
+            OutputCommand = new RelayCommand<object>((p) => { return true; }, (p) => { OutputWindow wd = new OutputWindow(); wd.ShowDialog(); } );
 
         }
     }
